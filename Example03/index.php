fb7597a4d4d48185435b3f4e899e696bd77bd1e3
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2021-02-06 18:25:33 +1300

    Create index.php

---
+++ Example03/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vuejs</title>
+	<script src="https://unpkg.com/vue/dist/vue.min.js"></script>
+</head>
+<body>
+<div id="msg">
+  {{ msgbody.fstr }} {{ msgbody.lstr }}<br>
+</div>
+	<script type = "text/javascript" src = "js/mainscript.js"></script>
+</body>
+</html>
